17ed5972404e2cd937f039966bc28a6a48a4d535
Thierry Raimond <thierry.raimond@creativity-studios.com>
2022-08-18 11:56:59 +0200

    Upgrade to Webpack5 and ESLint installed DJP4-17

---
+++ src/components/domLinker.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // DOM Elements
+  myTopNav: document.getElementById('myTopnav'),
+  editNavIcon: document.getElementById('edit-nav'),
+  modalbg: document.querySelector('.bground'),
+  modalBtn: document.querySelectorAll('.modal-btn'),
+  formData: document.querySelectorAll('.formData'),
+  modalClose: document.querySelectorAll('.close-modal'),
+  modalBtnSubmit: document.querySelectorAll('.btn-submit'),
+  form: document.querySelector('form'),
+  inputFirst: document.getElementById('first'),
+  inputLast: document.getElementById('last'),
+  inputEmail: document.getElementById('email'),
+  inputBirthdate: document.getElementById('birthdate'),
+  inputQuantity: document.getElementById('quantity'),
+  inputsRadio: document.querySelectorAll('input[type=radio]'),
+  inputTermOfUse: document.getElementById('checkbox1'),
+  modalConfirmation: document.getElementById('confirmation')
+}
